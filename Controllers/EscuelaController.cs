767e78889f7758c780bbf7f88353f93ccfda7e67
Israel Calderon <icalderond@outlook.com>
2019-04-14 12:19:15 -0500

    Configuring context and use

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,23 +1,31 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using net.practices.aspnetcore.Models;
+using System.Linq;
 
 namespace net.practices.aspnetcore.Controllers
 {
     public class EscuelaController : Controller
     {
+        private EscuelaContext _context;
+        public EscuelaController(EscuelaContext context)
+        {
+            _context=context;
+        }
         public IActionResult Index()
         {
-            var escuela = new Escuela();
-            escuela.AñoDeCreación = 2005;
-            escuela.UniqueId = Guid.NewGuid().ToString();
-            escuela.Nombre = "IDA Schoool";
-            escuela.Ciudad="Reforma";
-            escuela.Pais="México";
-            escuela.TipoEscuela=TiposEscuela.Secundaria;
-            escuela.Dirección="Avenida siempre viva 3245";
+            // var escuela = new Escuela();
+            // escuela.AñoDeCreación = 2005;
+            // escuela.UniqueId = Guid.NewGuid().ToString();
+            // escuela.Nombre = "IDA Schoool";
+            // escuela.Ciudad="Reforma";
+            // escuela.Pais="México";
+            // escuela.TipoEscuela=TiposEscuela.Secundaria;
+            // escuela.Dirección="Avenida siempre viva 3245";
 
-            return View(escuela);
+            var escuelaFromDb=_context.Escuelas.FirstOrDefault();
+
+            return View(escuelaFromDb);
         }
     }
 }
