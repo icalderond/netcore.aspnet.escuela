03fab5cd7aa0543b062eba9eaf9a2d53e451c183
Israel Calderon <icalderond@outlook.com>
2019-04-14 18:24:02 -0500

    Routing

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -14,9 +14,23 @@ namespace net.practices.aspnetcore.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        // public IActionResult Index()
+        // {
+        //     return View(_context.Asignaturas.FirstOrDefault());
+        // }
+
+        [Route("Asignatura/Index")]
+        [Route("Asignatura/Index/{asignaturaId}")]
+        public IActionResult Index(string asignaturaId)
         {
-            return View(_context.Asignaturas.FirstOrDefault());
+            if (!string.IsNullOrEmpty(asignaturaId))
+            {
+                var asignatura = _context.Asignaturas
+                .Where(x => x.Id == asignaturaId).SingleOrDefault();
+                return View(asignatura);
+            }else{
+                return View("MultiAsignatura",_context.Asignaturas.ToList());
+            }
         }
 
         public IActionResult MultiAsignatura()
