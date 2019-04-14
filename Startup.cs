37344a2a9fd9972d8ec29456302542cfb743238a
Israel Calderon <icalderond@outlook.com>
2019-04-13 22:57:52 -0500

    Razor syntaxis

--- Startup.cs
+++ Startup.cs
@@ -57,7 +57,7 @@ namespace net.practices.aspnetcore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Escuela}/{action=Index}/{id?}");
             });
         }
     }
