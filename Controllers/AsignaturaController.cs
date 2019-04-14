9f0a075d33cad74a4483b01edc7e03c94ad70a68
Israel Calderon <icalderond@outlook.com>
2019-04-14 10:06:46 -0500

    Partial Views, foreach

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using net.practices.aspnetcore.Models;
 
@@ -12,9 +13,25 @@ namespace net.practices.aspnetcore.Controllers
             asignatura.UniqueId = Guid.NewGuid().ToString();
             asignatura.Nombre = "Matematicas";
 
-            ViewBag.Fecha=DateTime.UtcNow;
+            ViewBag.Fecha = DateTime.UtcNow;
 
             return View(asignatura);
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas",
+                            UniqueId=Guid.NewGuid().ToString()} ,
+                            new Asignatura{Nombre="Educación Física",
+                            UniqueId=Guid.NewGuid().ToString()},
+                            new Asignatura{Nombre="Castellano",
+                            UniqueId=Guid.NewGuid().ToString()},
+                            new Asignatura{Nombre="Ciencias Naturales",
+                            UniqueId=Guid.NewGuid().ToString()}
+                };
+
+            return View(listaAsignaturas);
         }
     }
 }
