34ed28073044b62fbabb6a023931330b6231f4b0
Israel Calderon <icalderond@outlook.com>
2019-04-18 16:25:12 -0500

    Create and Edit Alumno finished

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -34,5 +34,55 @@ namespace net.practices.aspnetcore.Controllers
         {
             return View(_context.Alumnos.ToList());
         }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Alumnos.Add(alumno);
+                _context.SaveChanges();
+
+                return View("Index", alumno);
+            }
+            else
+            {
+                return View(alumno);
+            }
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var alumnoResult = from alumno in _context.Alumnos
+                                   where alumno.Id == id
+                                   select alumno;
+            return View(alumnoResult.SingleOrDefault());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                var alumnoToEdit = _context.Alumnos
+                .FirstOrDefault(x => x.Id == alumno.Id);
+
+                alumnoToEdit.Nombre = alumno.Nombre;
+
+                _context.Alumnos.Update(alumnoToEdit);
+                _context.SaveChanges();
+
+                return View("Index", alumno);
+            }
+            else
+            {
+                return View(alumno);
+            }
+        }
     }
 }
